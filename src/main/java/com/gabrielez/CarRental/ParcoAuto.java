443666e1a81151f53444c270ad814bdc5f089c23
Gabriele Zarcone <gabriele.zarcone0@gmail.com>
2021-01-25 17:43:48 +0100

    Pagina Parco Auto

---
+++ src/main/java/com/gabrielez/CarRental/ParcoAuto.java
@@ -0,0 +1,21 @@
+package com.gabrielez.CarRental;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "ParcoAuto", value = "/ParcoAuto")
+public class ParcoAuto extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.setAttribute("pagina", "parcoAuto.jsp");
+        RequestDispatcher dispatcher = request.getRequestDispatcher("index.jsp");
+        dispatcher.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
